28817f7c7dc9db820e1594a57b593df357968582
Luana Stephany <ls.luanastephany@gmail.com>
2021-08-06 21:32:36 -0300

    componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Produtos from './Components/Produtos'
 
 function App() {
   return (
     <div>
-
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={Produtos} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }

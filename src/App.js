38e9690e58bd21916522188b1ed82071125e0a57
Luana Stephany <ls.luanastephany@gmail.com>
2021-08-11 12:08:32 -0300

    path para produto

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Produtos from './Components/Produtos'
 import Header from './Components/Header'
 import Contato from './Components/Contato'
 import Footer from './Components/Footer'
+import Produto from './Components/Produto'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <div className="content">
           <Switch>
             <Route path="/" exact component={Produtos} />
+            <Route path="/produto/:id" exact component={Produto} />
             <Route path="/contato" component={Contato} />
           </Switch>
         </div>
